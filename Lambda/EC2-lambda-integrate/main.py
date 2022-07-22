547e8dd8ba77e21ac295fd6ee820570da309b1d5
Tanmay Shukla <96629547+tanmaycode2@users.noreply.github.com>
2022-07-22 16:52:16 +0530

    Create main.py

---
+++ Lambda/EC2-lambda-integrate/main.py
@@ -0,0 +1,26 @@
+#Start and Stop Ec2 Instance via Lambda function
+
+#1) function code(python 3.9) for starting EC2 instances
+
+import boto3
+region = 'us-west-1'
+instances = ['i-12345cb6de4f78g9h', 'i-08ce9b2d7eccf6d26']
+ec2 = boto3.client('ec2', region_name=region)
+
+def lambda_handler(event, context):
+    ec2.start_instances(InstanceIds=instances)
+    print('started your instances: ' + str(instances))
+
+
+
+
+#2) function code for stopping EC2 instances
+
+import boto3
+region = 'us-west-1'
+instances = ['i-12345cb6de4f78g9h', 'i-08ce9b2d7eccf6d26']
+ec2 = boto3.client('ec2', region_name=region)
+
+def lambda_handler(event, context):
+    ec2.stop_instances(InstanceIds=instances)
+    print('stopped your instances: ' + str(instances))
